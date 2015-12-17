efdc6976b1205d4f21276cb30e76ea34d47b1ce5
212044552 <spliskamatyshak@almebezbik.com>
2015-12-17 14:30:11 -0600

    directory creation ONCE!

--- recipes/createdirs.rb
+++ recipes/createdirs.rb
@@ -7,56 +7,57 @@
 # All rights reserved - Do Not Redistribute
 #
 
-rusr = node['oracle-omni']['rdbms']['user']
-rgrp = node['oracle-omni']['rdbms']['groups'].keys.first
-gusr = node['oracle-omni']['grid']['user']
-ggrp = node['oracle-omni']['grid']['groups'].keys.first
-mnt = node['oracle-omni']['oracle']['home_mount']
+unless node.attribute?('dirs_created')
 
-[
-  node['oracle-omni']['rdbms']['oracle_base'],
-  node['oracle-omni']['grid']['oracle_home'],
-  node['oracle-omni']['oracle']['oracle_inventory'],
-  node['oracle-omni']['rdbms']['oracle_home']
-].each do |dirs|
-  directory dirs do
+  rusr = node['oracle-omni']['rdbms']['user']
+  rgrp = node['oracle-omni']['rdbms']['groups'].keys.first
+  gusr = node['oracle-omni']['grid']['user']
+  ggrp = node['oracle-omni']['grid']['groups'].keys.first
+  mnt = node['oracle-omni']['oracle']['home_mount']
+
+  [
+    node['oracle-omni']['rdbms']['oracle_base'],
+    node['oracle-omni']['grid']['oracle_home'],
+    node['oracle-omni']['oracle']['oracle_inventory'],
+    node['oracle-omni']['rdbms']['oracle_home']
+  ].each do |dirs|
+    directory dirs do
+      owner gusr
+      group ggrp
+      mode '0775'
+      recursive true
+    end
+  end
+
+  directory node['oracle-omni']['oracle']['install_dir'] do
     owner gusr
     group ggrp
     mode '0775'
     recursive true
+    not_if { node['oracle-omni']['oracle']['files_url'].nil? }
   end
-end
 
-directory node['oracle-omni']['oracle']['install_dir'] do
-  owner gusr
-  group ggrp
-  mode '0775'
-  recursive true
-  not_if { node['oracle-omni']['oracle']['files_url'].nil? }
-end
+  directory node['oracle-omni']['oracle']['patch_dir'] do
+    owner gusr
+    group ggrp
+    mode '0775'
+    recursive true
+    not_if { node['oracle-omni']['oracle']['patch_dir'].empty? }
+  end
 
-directory node['oracle-omni']['oracle']['patch_dir'] do
-  owner gusr
-  group ggrp
-  mode '0775'
-  recursive true
-  not_if { node['oracle-omni']['oracle']['patch_dir'].empty? }
-end
+  execute 'set_grid_owner' do
+    command "chown -R #{gusr}:#{ggrp} #{mnt}"
+  end
 
-execute 'set_grid_owner' do
-  command "chown -R #{gusr}:#{ggrp} #{mnt}"
-  not_if "ls -ld #{mnt} | grep #{gusr} | grep -q #{ggrp}"
-end
+  execute 'set_home_mount_mode' do
+    command "chmod -R 775 #{mnt}/"
+  end
 
-execute 'set_home_mount_mode' do
-  command "chmod -R 775 #{mnt}/"
-  not_if "ls -ld #{mnt} | grep -q drwxrwxr-x"
-end
+  execute 'oracle_ownership' do
+    command "chown -R #{rusr}:#{rgrp} #{node['oracle-omni']['rdbms']['oracle_base']}"
+  end
 
-# Create rdbms home
+  node.normal['dirs_created'] = true
+  node.save
 
-execute 'oracle_ownership' do
-  command "chown -R #{rusr}:#{rgrp} \
-  #{node['oracle-omni']['rdbms']['oracle_base']}"
-  not_if "ls -ld #{node['oracle-omni']['rdbms']['oracle_base']} | grep -q \"#{rusr} #{rgrp}\""
 end
