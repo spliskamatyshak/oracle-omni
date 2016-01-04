da68bc4f8960dc43c4d75c926d277e013eb2dee8
212044552 <spliskamatyshak@almebezbik.com>
2016-01-04 15:14:26 -0600

    license and urls in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,11 @@
 name             'oracle-omni'
 maintainer       'Stephen Pliska-Matyshak'
 maintainer_email 'spliskamatyshak@almebezbik.com'
-license          'All rights reserved'
+license          'Apache v2.0'
 description      'Installs/Configures oracle-omni'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+source_url       'https://github.com/spliskamatyshak/oracle-omni'
+issues_url       'https://github.com/spliskamatyshak/oracle-omni/issues'
 version          '0.3.16'
 
 supports         'oracle'
